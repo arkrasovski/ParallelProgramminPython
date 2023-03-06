9784491aabf638e472a0f1a364b892db084f0289
Artyom <arkrasovski1996@gmail.com>
2023-03-07 00:11:19 +0300

    fix: fix lab1 and kill it at the same time

--- lab1/main.py
+++ lab1/main.py
@@ -10,23 +10,23 @@ kek.show_eq()
 # [x1, x2] = kek.find_roots()
 # print(x1, x2)
 
+size = 100000
+portion = 20
+
 # range(start, stop, step)
 # range(4)        # [0, 1, 2, 3] 0 through 4, excluding 4
 # range(1, 4)     # [1, 2, 3] 1 through 4, excluding 4
 # range(1, 10, 2) # [1, 3, 5, 7, 9] 1 through 10, counting by 2s
-equations = [QeQ(random.randint(1, 100), random.randint(1, 100), random.randint(1, 100)) for i in range(10 ** 6)]
+equations = [QeQ(random.randint(1, 100), random.randint(1, 100), random.randint(1, 100)) for i in range(size)]
 
-size = 1000000
-portion = 10
 
 
 def solve_eq(arr, start, end):
 
     eq = arr[int(start):int(end)]
-    for i in range(0, int(end)-int(start), 1):
-        #print(i)
-
-        eq[i].find_roots()
+    for i in eq:
+        i.find_roots()
+        #time.sleep(0.1)
 
 # def worker(argument):
 #     print("поток замирает " + str(argument))
@@ -38,6 +38,21 @@ def solve_eq(arr, start, end):
 #     t = Thread(target=worker, args=[i])
 #     t.start()
 
-for i in range(portion):
-    th = Thread(target=solve_eq, args=(equations, i * size / portion, size / portion * (i + 1), ))
-    th.start()
+def main(portion):
+    threads = [
+        Thread(target=solve_eq, args=(equations, i * size / portion, size / portion * (i + 1),))
+        for i in range(0, portion)
+    ]
+    print(len(threads))
+    
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+for i in range(2, portion + 1, 2):
+    start_time = time.time()
+    main(i)
+    end_time = time.time()
+    print("for ", i, " number of threads time is ", end_time-start_time)
